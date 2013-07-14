4de8683112d37d5085e63e7bdd39c16c28a7bb50
jessica_fang88@hotmail.com <jessica_fang88@hotmail.com>
2013-07-14 21:35:43 +0800

    Initial Commit

---
+++ Sick Notification/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sick Notification
+//
+//  Created by Lim Jessica on 14/7/13.
+//  Copyright (c) 2013 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sick Notification/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sick Notification
+//
+//  Created by Lim Jessica on 14/7/13.
+//  Copyright (c) 2013 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
